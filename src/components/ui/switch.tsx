1e1bbea46d65e84d1a85adc151257f75550fce08
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2023-10-16 15:47:13 +0530

    chore: wahyu sucks
    permalink: http://whatthecommit.com/0a95e95262a10379dc986dabcc217c99

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -11,7 +11,7 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
 	<SwitchPrimitives.Root
 		className={cn(
-			"switch peer inline-flex h-[28px] w-[80px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-secondary data-[state=unchecked]:bg-input",
+			"switch peer inline-flex h-[28px] w-[80px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-input data-[state=unchecked]:bg-input",
 			className
 		)}
 		{...props}

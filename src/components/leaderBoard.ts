bc2a7bbd3d4847d477fde68a379ea3c0313bfab9
EvolutionX-10 <EvolutionX-10@users.noreply.github.com>
2023-10-17 13:55:37 +0000

    chore: update leaderboard
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/components/leaderBoard.ts
+++ src/components/leaderBoard.ts
@@ -1145,4 +1145,4 @@ export const leaderBoardData = [
     }
 ];
 
-export const updateTime =  "Tuesday, October 17, 2023 at 7:14 PM"
+export const updateTime =  "Tuesday, October 17, 2023 at 7:25 PM"

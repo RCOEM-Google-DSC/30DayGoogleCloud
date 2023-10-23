a83ddf3d8fc476a2ee56a474e20e746b34a1209a
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2023-10-23 15:35:07 +0530

    chore: minor changes

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -10,11 +10,15 @@ export function Details() {
 				<div className="flex justify-around max-md:flex-col">
 					<div className="blue flex h-8 items-center pl-4 text-primary max-md:border-b-[1px]">
 						Both Tracks{' '}
-						<span className="ml-1 flex w-5 items-center justify-center rounded-md bg-blue-300 px-1">{totalBlue}</span>
+						<span className="ml-1 flex w-5 items-center justify-center rounded-md bg-blue-300 px-1 text-black">
+							{totalBlue}
+						</span>
 					</div>
 					<div className="green flex h-8 items-center pl-4 text-primary">
 						Single Track{' '}
-						<span className="ml-1 flex w-5 items-center justify-center rounded-md bg-green-300 px-1">{totalGreen}</span>
+						<span className="ml-1 flex w-5 items-center justify-center rounded-md bg-green-300 px-1 text-black">
+							{totalGreen}
+						</span>
 					</div>
 				</div>
 				<span className="p-2 text-sm font-light text-gray-400 drop-shadow-md">Last Updated: {updateTime}</span>
